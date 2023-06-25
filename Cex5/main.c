8ebd48799d212e5994363d854272ef7322c06a28
Leesuyeoun <sssiuir@gmail.com>
2023-06-25 22:25:56 +0900

    Initial Commit

---
+++ Cex5/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Cex5
+//
+//  Created by 이수연 on 2023/06/25.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
